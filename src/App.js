71b2cee38d5b2be06097b2547c368c26c9aeed71
Trung <thanhtrung13111997@gmail.com>
2022-05-19 09:27:07 +0700

    upload source code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './assets/images/Banner.png';
+import BackgroundIcon from './assets/images/BackgroundIcon.svg';
+import BackgroundBottomRight from './assets/images/BackgroundBottomRight.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ display: 'flex', position: 'relative' }}>
+      <img src={Banner} alt='banner' style={{ zIndex: '999' }} />
+      <img
+        style={{ position: 'absolute', bottom: '2%', right: '-2%' }}
+        src={BackgroundIcon}
+        alt='background'
+      />
+      <img
+        style={{ position: 'absolute', bottom: '0', right: '0' }}
+        src={BackgroundBottomRight}
+        alt='background'
+      />
+
+      <div>
+        <div className='sign-up-heading'>Enter To Win</div>
+        <div className='sign-up-title'>SignUp Form</div>
+        <div className='sign-up-description'>
+          Enter our giveaway for a chance to win
+        </div>
+        <div className='sign-up-date-time'>2 Days 23:13:29 Remaining</div>
+        <input />
+      </div>
     </div>
   );
 }
