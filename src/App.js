be54797199d0f22c40c49c29c9589a1613c12142
Trung <thanhtrung13111997@gmail.com>
2022-05-19 11:01:27 +0700

    reponsive mobile 414x896

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
 import './App.css';
+import 'antd/dist/antd.css';
 import Banner from './assets/images/Banner.png';
 import BackgroundIcon from './assets/images/BackgroundIcon.svg';
 import BackgroundBottomRight from './assets/images/BackgroundBottomRight.png';
+import BannerMobile from './assets/images/BannerMobile.png';
+import Facebook from './assets/images/Facebook.svg';
+import Instagram from './assets/images/Instagram.svg';
+import Twitter from './assets/images/Twitter.svg';
+
+import { Button, Col, Input, Row } from 'antd';
 
 function App() {
   return (
-    <div className='App' style={{ display: 'flex', position: 'relative' }}>
-      <img src={Banner} alt='banner' style={{ zIndex: '999' }} />
-      <img
-        style={{ position: 'absolute', bottom: '2%', right: '-2%' }}
-        src={BackgroundIcon}
-        alt='background'
-      />
-      <img
-        style={{ position: 'absolute', bottom: '0', right: '0' }}
-        src={BackgroundBottomRight}
-        alt='background'
-      />
+    <Row>
+      <Col xl={24}>
+        <div className='main-container'>
+          <img
+            className='sign-up-background-icon'
+            src={BackgroundIcon}
+            alt='background'
+          />
+          <img
+            className='sign-up-background-bottom-right'
+            src={BackgroundBottomRight}
+            alt='background'
+          />
+          <img
+            className='sign-up-banner-mobile'
+            src={BannerMobile}
+            alt='Background'
+          />
+          <Col xl={13}>
+            <img className='sign-up-banner-image' src={Banner} alt='banner' />
+          </Col>
 
-      <div>
-        <div className='sign-up-heading'>Enter To Win</div>
-        <div className='sign-up-title'>SignUp Form</div>
-        <div className='sign-up-description'>
-          Enter our giveaway for a chance to win
+          <Col xl={11}>
+            <div className='form-sign-up-container'>
+              <div className='sign-up-heading'>Enter To Win</div>
+              <div className='sign-up-title'>SignUp Form</div>
+              <div className='sign-up-description'>
+                Enter our giveaway for a chance to win
+              </div>
+              <div className='sign-up-date-time'>2 Days 23:13:29 Remaining</div>
+              <div className='input-container'>
+                <Input defaultValue={'Enter your email'} />
+              </div>
+              <div className='sign-up-btn-container'>
+                <Button>CLAIM</Button>
+              </div>
+
+              <div className='sign-up-share-contact'>
+                Share with your friends on
+              </div>
+              <div className='sign-up-contact-icon-container'>
+                <img src={Facebook} alt='icon' />
+                <img src={Instagram} alt='icon' />
+                <img src={Twitter} alt='icon' />
+              </div>
+            </div>
+          </Col>
         </div>
-        <div className='sign-up-date-time'>2 Days 23:13:29 Remaining</div>
-        <input />
-      </div>
-    </div>
+      </Col>
+    </Row>
   );
 }
 
